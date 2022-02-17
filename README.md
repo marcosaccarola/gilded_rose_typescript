a4f041f52cbf28c7fb07ffbfdc4c57392f332053
Marco Saccarola <marco.sacca.ads@gmail.com>
2022-02-17 20:00:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gilded_rose_typescript
+ 
---
+++ test/guilded-rose.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import { Item, GildedRose } from '../app/gilded-rose';
+
+describe('Gilded Rose', function () {
+
+    it('should foo', function() {
+        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].name).to.equal('foo');
+    });
+    it('is quality >=0?', function() {
+        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.be.above(-1);
+    });
+    it('is quality <=50?', function() {
+        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.be.below(51);
+    });
+
+});
