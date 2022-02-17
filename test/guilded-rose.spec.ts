b6c8a41f6cea71695ba8dd073fe5d71558d5a621
Marco Saccarola <marco.sacca.ads@gmail.com>
2022-02-17 20:28:37 +0100

    Update guilded-rose.spec.ts

--- test/guilded-rose.spec.ts
+++ test/guilded-rose.spec.ts
@@ -8,15 +8,5 @@ describe('Gilded Rose', function () {
         const items = gildedRose.updateQuality();
         expect(items[0].name).to.equal('foo');
     });
-    it('is quality >=0?', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].quality).to.be.above(-1);
-    });
-    it('is quality <=50?', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].quality).to.be.below(51);
-    });
 
 });
