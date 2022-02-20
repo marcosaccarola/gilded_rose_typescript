5e52e050024ca42f35bdddd7ed34b58aed1d58d0
Marco Saccarola <marco.sacca.ads@gmail.com>
2022-02-20 17:51:21 +0100

    Java: merged conditional branches

--- java/src/main/java/com/gildedrose/GildedRose.java
+++ java/src/main/java/com/gildedrose/GildedRose.java
@@ -17,6 +17,12 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality += 1;
                 }
+                if (items[i].sellIn < 1) {
+                    if (items[i].quality < 50) {
+                        items[i].quality += 1;
+                    }
+                }
+
             } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality < 50) {
                     items[i].quality += 1;
@@ -32,20 +38,15 @@ class GildedRose {
                         }
                     }
                 }
+                if (items[i].sellIn < 1) {
+                    items[i].quality = 0;
+                }
+
             } else {
                 if (items[i].quality > 0) {
                     items[i].quality -= 1;
                 }
-            }
-
-            if (items[i].sellIn < 1) {
-                if (items[i].name.equals("Aged Brie")) {
-                    if (items[i].quality < 50) {
-                        items[i].quality += 1;
-                    }
-                } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    items[i].quality = 0;
-                } else {
+                if (items[i].sellIn < 1) {
                     if (items[i].quality > 0) {
                         items[i].quality -= 1;
                     }
