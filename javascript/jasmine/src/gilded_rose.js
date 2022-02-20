b45a4778aae1ddc4976d5760b5a1d5feb3d6dbde
Marco Saccarola <marco.sacca.ads@gmail.com>
2022-02-20 14:14:13 +0100

    Javascript Jasmine: just 2 conditional levels

--- javascript/jasmine/src/gilded_rose.js
+++ javascript/jasmine/src/gilded_rose.js
@@ -26,16 +26,14 @@ class Shop {
       } else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality < 50) {
           this.items[i].quality += 1;
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality += 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality += 1;
-              }
-            }
+        }
+        if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality += 1;
+          }
+        }
+        if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
+            this.items[i].quality += 1;
         }
         if (this.items[i].sellIn < 1) {
           this.items[i].quality = 0;
