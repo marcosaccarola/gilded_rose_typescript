c3d57bbb3e580f1ffffbc4fc39ceb7fdc6904b79
Marco Saccarola <marco.sacca.ads@gmail.com>
2022-02-20 13:37:03 +0100

    Javascript Jasmine: Sulfuras logic

--- javascript/jasmine/src/gilded_rose.js
+++ javascript/jasmine/src/gilded_rose.js
@@ -12,6 +12,10 @@ class Shop {
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
+      if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') {
+        continue;
+      }
+
       if (this.items[i].name == 'Aged Brie') {
         if (this.items[i].quality < 50) {
           this.items[i].quality += 1;
@@ -32,17 +36,11 @@ class Shop {
         }
       } else {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality -= 1;
-          }
         }
       }
 
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn -= 1;
-      }
-
-      if (this.items[i].sellIn < 0) {
+      if (this.items[i].sellIn < 1) {
         if (this.items[i].name == 'Aged Brie') {
           if (this.items[i].quality < 50) {
             this.items[i].quality += 1;
@@ -51,12 +49,11 @@ class Shop {
             this.items[i].quality = 0;
         } else {
           if (this.items[i].quality > 0) {
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
               this.items[i].quality -= 1;
-            }
           }
         }
       }
+      this.items[i].sellIn -= 1;
     }
 
     return this.items;
