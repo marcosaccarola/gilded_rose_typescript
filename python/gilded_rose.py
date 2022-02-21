1853f1fc98f0d805d11052b6546506b1cd3504f0
Marco Saccarola <marco.sacca.ads@gmail.com>
2022-02-21 22:53:44 +0100

    Python: 2 conditional levels

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -13,28 +13,22 @@ class GildedRose(object):
             if item.name == "Aged Brie":
                 if item.quality < 50:
                     item.quality += 1
+                if item.sell_in < 1 and item.quality < 50:
+                    item.quality += 1
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
                     item.quality += 1
-                if item.sell_in < 11:
-                    if item.quality < 50:
-                        item.quality += 1
-                if item.sell_in < 6:
-                    if item.quality < 50:
-                        item.quality += 1
+                if item.sell_in < 11 and item.quality < 50:
+                    item.quality += 1
+                if item.sell_in < 6 and item.quality < 50:
+                    item.quality += 1
+                if item.sell_in < 1:
+                    item.quality = 0
             else:
                 if item.quality > 0:
                     item.quality -= 1
-
-            if item.sell_in < 1:
-                if item.name == "Aged Brie":
-                    if item.quality < 50:
-                        item.quality += 1
-                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    item.quality = 0
-                else:
-                    if item.quality > 0:
-                        item.quality -= 1
+                if item.sell_in < 1 and item.quality > 0:
+                    item.quality -= 1
 
             item.sell_in -= 1
 
