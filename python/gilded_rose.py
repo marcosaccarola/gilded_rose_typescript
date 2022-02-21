9ef483a8bf4ac4af5e0996abb7aa37d316ce8392
Marco Saccarola <marco.sacca.ads@gmail.com>
2022-02-21 22:48:24 +0100

    Python: 3 conditional levels

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -10,16 +10,18 @@ class GildedRose(object):
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
 
-            if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
+            if item.name == "Aged Brie":
                 if item.quality < 50:
                     item.quality += 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality += 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality += 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality < 50:
+                    item.quality += 1
+                if item.sell_in < 11:
+                    if item.quality < 50:
+                        item.quality += 1
+                if item.sell_in < 6:
+                    if item.quality < 50:
+                        item.quality += 1
             else:
                 if item.quality > 0:
                     item.quality -= 1
@@ -28,12 +30,11 @@ class GildedRose(object):
                 if item.name == "Aged Brie":
                     if item.quality < 50:
                         item.quality += 1
+                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    item.quality = 0
                 else:
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        item.quality = 0
-                    else:
-                        if item.quality > 0:
-                            item.quality -= 1
+                    if item.quality > 0:
+                        item.quality -= 1
 
             item.sell_in -= 1
 
