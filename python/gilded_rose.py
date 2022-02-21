abf5f63eef654013c5510db91a1123fe6e47e7fd
Marco Saccarola <marco.sacca.ads@gmail.com>
2022-02-21 22:43:16 +0100

    Python: syntax

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -12,30 +12,30 @@ class GildedRose(object):
 
             if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    item.quality += 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality += 1
                         if item.sell_in < 6:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality += 1
             else:
                 if item.quality > 0:
-                    item.quality = item.quality - 1
+                    item.quality -= 1
 
             if item.sell_in < 1:
                 if item.name == "Aged Brie":
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        item.quality += 1
                 else:
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                     else:
                         if item.quality > 0:
-                            item.quality = item.quality - 1
+                            item.quality -= 1
 
-            item.sell_in = item.sell_in - 1
+            item.sell_in -= 1
 
 
 class Item:
